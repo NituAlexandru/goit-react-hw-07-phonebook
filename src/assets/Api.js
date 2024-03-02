51737d2070317b077b0acb1f5a215e14465d1943
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2024-03-02 19:59:02 +0200

    Update Api.js

--- src/assets/Api.js
+++ src/assets/Api.js
@@ -3,10 +3,26 @@ import axios from "axios";
 const BASE_URL = "https://65e326e088c4088649f5677f.mockapi.io/api/v1";
 
 const api = {
-  fetchContacts: () => axios.get(`${BASE_URL}/contacts`),
-  addContact: (contact) => axios.post(`${BASE_URL}/contacts`, contact),
-  deleteContact: (contactId) =>
-    axios.delete(`${BASE_URL}/contacts/${contactId}`),
+  fetchContacts: () => {
+    return axios.get(`${BASE_URL}/contacts`).then((response) => {
+      console.log("fetchContacts response:", response);
+      return response;
+    });
+  },
+  addContact: (contact) => {
+    return axios.post(`${BASE_URL}/contacts`, contact).then((response) => {
+      console.log("addContact response:", response);
+      return response;
+    });
+  },
+  deleteContact: (contactId) => {
+    return axios
+      .delete(`${BASE_URL}/contacts/${contactId}`)
+      .then((response) => {
+        console.log("deleteContact response:", response);
+        return response;
+      });
+  },
 };
 
 export default api;
